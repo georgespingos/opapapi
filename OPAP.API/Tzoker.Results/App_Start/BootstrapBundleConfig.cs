dd033485d03d7943344c534fcf8ab698a6dae22d
georgespingos <exavier321>
2013-03-01 21:20:16 +0200

    Added live testing

--- OPAP.API/Tzoker.Results/App_Start/BootstrapBundleConfig.cs
+++ OPAP.API/Tzoker.Results/App_Start/BootstrapBundleConfig.cs
@@ -9,19 +9,23 @@ namespace BootstrapSupport
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/js").Include(
-                "~/Scripts/jquery-{version}.js",
+                 "~/Scripts/jquery-{version}.js",
                 "~/Scripts/jquery-migrate-{version}.js",
                 "~/Scripts/bootstrap.js",
                 "~/Scripts/jquery.validate.js",
                 "~/scripts/jquery.validate.unobtrusive.js",
-                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js"
+                "~/Scripts/jquery.validate.unobtrusive-custom-for-bootstrap.js",
+                "~/Scripts/knockout-2.2.1.js",
+                "~/Scripts/MainApp.js",
+                "~/Scripts/getData.js",
+                "~/Scripts/jquery.blockUI.js"
                 ));
 
             bundles.Add(new StyleBundle("~/content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/body.css",
-                "~/Content/bootstrap-responsive.css",
-                "~/Content/bootstrap-mvc-validation.css"
+                "~/Content/bootstrap.css"
+                ));
+            bundles.Add(new StyleBundle("~/content/css-responsive").Include(
+                "~/Content/bootstrap-responsive.css"
                 ));
         }
     }
